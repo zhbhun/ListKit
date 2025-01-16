a8f6d06dd011477c4de41fdd9d352041dc9979c5
zhbhun <zhbhun@gmail.com>
2025-01-16 21:10:15 +0800

    fix: setup deploy branch

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -14,11 +14,13 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ListKit/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'zhbhun', // Usually your GitHub org/user name.
   projectName: 'listkit', // Usually your repo name.
+  deploymentBranch: "website",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
